90d19a25618b8e105e86210b20794b7097540c8f
UserBryan <bkk@gmail.com>
2024-02-10 15:15:45 -0800

    Console 0.0.1

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Script for the project console"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print("")  # Print a newline for better formatting
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
